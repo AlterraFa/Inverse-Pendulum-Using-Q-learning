a36e7e0e865715b7c1e3a9fd535d051729ce194a
Alterra Onix <ntony8124@gmail.com>
2024-09-06 23:40:12 +0700

    Refined the system and added friction

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,51 @@
 #include "obj.hpp"
 #include <cmath>
 
-
-std::vector<float> funct(std::vector<float> x, float accel){
+std::vector<float> funct(std::vector<float> x, float M, float m, float length, float force, float forceS){
     float xdot = x[1];
-    float xddot = accel;
-    return {xdot, xddot};
+    float thetadot = x[3];
+
+    float a = M + m;
+    float b = m * length * cosf(x[2]);
+    float c = force - m * length * powf(thetadot, 2) * sinf(x[2]);
+    float d = length;
+    float e = cosf(x[2]);
+    float f = conf::cgravity * sinf(x[2]) + forceS;
+
+    float xddot = ((c / a) + ((b * f) / (a * d))) / (1 - ((b * e) / (a * d)));
+    float thetaddot = (f + e * xddot) / d;
+
+    return {xdot, xddot, thetadot, thetaddot};
 }
 
-void forceRungeKutta(std::vector<float>& x, float accel, float timeStep){
-    std::vector<float> f1 = funct(x, accel);
+std::vector<float> vectorPushback(std::vector<float> x, std::vector<float> funct, float timeStep, float coeff){
+    std::vector<float> res;
+    for (size_t i = 0; i < x.size(); i++){
+        res.push_back(x[i] + coeff * timeStep * funct[i]);
+    }
+    return res;
+}
 
-    std::vector<float> x2 = {x[0] + timeStep * f1[0] * .5f, x[1] + timeStep * f1[1] * .5f};
-    std::vector<float> f2 = funct(x2, accel);
+void stateUpdate(std::vector<float>& x, float M, float m, float length, float force, float forceS, float timeStep){
 
-    std::vector<float> x3 = {x[0] + timeStep * f2[0] * .5f, x[1] + timeStep * f2[1] * .5f};
-    std::vector<float> f3 = funct(x3, accel);
+    std::vector<float> f1 = funct(x, M, m, length, force, forceS);
 
-    std::vector<float> x4 = {x[0] + timeStep * f3[0], x[1] + timeStep * f3[1]};
-    std::vector<float> f4 = funct(x4, accel);
+    std::vector<float> x2 = vectorPushback(x, f1, timeStep, .5f);
+    std::vector<float> f2 = funct(x2, M, m, length, force, forceS);
 
-    std::cout << f1[1] << " " << f2[1] << " " << f3[1] << " " << f4[1] << '\n';
-    x[0] += (1.f/6.f) * timeStep * (f1[0] + 2 * f2[0] + 2 * f3[0] + f4[0]);
-    x[1] += (1.f/6.f) * timeStep * (f1[1] + 2 * f2[1] + 2 * f3[1] + f4[1]);
+    std::vector<float> x3 = vectorPushback(x, f2, timeStep, .5f);
+    std::vector<float> f3 = funct(x3, M, m, length, force, forceS);
+
+    std::vector<float> x4 = vectorPushback(x, f3, timeStep, 1.f);
+    std::vector<float> f4 = funct(x4, M, m, length, force, forceS);
+
+    for (size_t i = 0; i < f1.size(); i ++){
+        x[i] += (1.f / 6.f) * timeStep * (f1[i] + 2 * f2[i] + 2 * f3[i] + f4[i]);
+    }
 }
 
 
-int main() {
+signed main() {
 
 
     // For esthetic only
@@ -41,58 +60,73 @@ int main() {
     std::tie(leftCircle, rightCircle) = cap(12.5f, 7.5f, std::make_tuple(925, 532.5), 1350);
     // For esthetic only
 
+    float inputForce = 350;
 
-    Rectangle cart(150, 50);
-    float cartMass = 10;
-    float inputForce = 100;
-    sf::Vector2f cartPosition = sf::Vector2f(800, 530);
+    Rectangle cart(150, 20);
+    float cartMass = 50;
     float cartVel = 0;
+    cart.setPosition(sf::Vector2f(900, 530));
 
-    float theta = 0;
+
+
+    Circle pendulum(30);
+    float pendulumMass = 10.5;
+    float angularVel = 0;
+    float theta = M_PI;
     float length = 300;
-    Circle pendulum(20);
-    sf::Vector2f pendulumPosition;
-    Circle point(5);
+    
+    Circle pivot(15);  
+    Rectangle rod(length, 6); 
 
-
+    sf::Vector2f newCartPos = cart.getPosition(), newPendulumPos, newRodPos;
     int inputType;
     while (window.isOpen()) {
         processEvents(window, inputType);        
 
-                 
-        float a = (inputType == RIGHT)? inputForce / cartMass: (inputType == LEFT)? -inputForce / cartMass: 0;
-        std::vector<float> x = {cartPosition.x, cartVel};
-        forceRungeKutta(x, a, conf::timeStep);
+        float force = (inputType == RIGHT && !(newCartPos.x >= std::get<1>(conf::railBound)))? inputForce: (inputType == LEFT && !(newCartPos.x <= std::get<0>(conf::railBound)))? -inputForce: 0;
+        float friction = -((cartVel > 0) - (cartVel < 0)) * 5;
+        float angularFriction = -((angularVel > 0) - (angularVel < 0)) * .075;
 
-        
-        cartPosition.x = x[0];
-        cartVel = x[1];
+        std::vector<float> stateVar = {cart.getPosition().x, cartVel, theta, angularVel};
+        stateUpdate(stateVar, cartMass, pendulumMass, length, force + friction, angularFriction, conf::timeStep);
 
-        if (cartPosition.x<= std::get<0>(conf::railBound)){
+        newCartPos = sf::Vector2f(stateVar[0], cart.getPosition().y);
+        cartVel = stateVar[1];
+        theta = stateVar[2];
+        angularVel = stateVar[3];
+        newPendulumPos = sf::Vector2f(newCartPos.x - length * sinf(theta), newCartPos.y - length * cosf(theta));
+
+        std::cout << cartVel << "\n";
+
+        if (newCartPos.x <= std::get<0>(conf::railBound)){
             cartVel = 0;
-            cartPosition.x = std::get<0>(conf::railBound);
-        } else if (cartPosition.x >= std::get<1>(conf::railBound)){
-            cartVel = 0;
-            cartPosition.x = std::get<1>(conf::railBound);
+            newCartPos.x = std::get<0>(conf::railBound);
         }
-        cart.setPosition(cartPosition);
-        sf::Vector2f cartPosition = cart.getPosition();
+        else if (newCartPos.x >= std::get<1>(conf::railBound)){
+            cartVel = 0;
+            newCartPos.x = std::get<1>(conf::railBound);
+        }
 
-        theta += 0.1 * conf::timeStep;
-        pendulumPosition.x = cartPosition.x - length * sinf(theta);
-        pendulumPosition.y = cartPosition.y - length * cosf(theta);
-        pendulum.setPosition(pendulumPosition);
+        newRodPos.x = (newCartPos.x + newPendulumPos.x) / 2;
+        newRodPos.y = (newCartPos.y + newPendulumPos.y) / 2;
+
+        // Update position
+        cart.setPosition(newCartPos);        
+        pendulum.setPosition(newPendulumPos);
+        pivot.setPosition(newCartPos);
+        rod.setPosition(newRodPos);
+        rod.setRotation(-theta * 180 / M_PI + 90);
 
 
-        point.setPosition(cartPosition);        
         window.clear(sf::Color(50, 50, 50));
         window.draw(cart);
         window.draw(upperRail);
         window.draw(lowerRail);
         window.draw(rightCircle);
         window.draw(leftCircle);
+        window.draw(rod);
         window.draw(pendulum);
-        window.draw(point);
+        window.draw(pivot);
         window.display();
     }
 
