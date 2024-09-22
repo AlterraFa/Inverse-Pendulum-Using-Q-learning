Fixed softmax, celoss and implemented celoss with derivative of sigmoid implemented

pysrc/listener.py
@@ -1,92 +1,75 @@
-import subprocess
 from pynput import keyboard
-import os
 import threading
 import numpy as np
+import subprocess
 
 
-build_dir = "build"
-exec_path = os.path.join(build_dir, "bin", "PendulumExec")
+class Listener():
+    def __init__(self, process) -> None:
+        self.process = process
+        self.data = None
+        output_thread = threading.Thread(target = self.read_output, args = ())
+        output_thread.daemon = True
+        output_thread.start()
 
-try:
-    os.makedirs(build_dir, exist_ok=True)
-    subprocess.run(["cmake", ".."], cwd=build_dir, check=True)
-    subprocess.run(["cmake", "--build", ".", f"-j{os.cpu_count()}"], cwd=build_dir, check=True)
-    print("Build successful.")
-except subprocess.CalledProcessError as e:
-    print(f"Error during CMake build: {e}")
-    exit(1)
+    def get_focused_window_title(self):
+        try:
+            result = subprocess.run(['xdotool', 'getwindowfocus', 'getwindowname'], capture_output=True, text=True)
+            return result.stdout.strip()
+        except Exception as e:
+            print(f"Error getting focused window: {e}")
+            return ""
 
-process = subprocess.Popen(
-    [exec_path],  
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    text=True
-)
+    def on_press(self, key):
+        if self.get_focused_window_title() == "Pendulum simulation":
+            if self.process.stdin:
+                try:
+                    if key == keyboard.Key.up:
+                        self.process.stdin.write("1\n")
+                    elif key == keyboard.Key.down:
+                        self.process.stdin.write("2\n")
+                    elif key == keyboard.Key.left:
+                        self.process.stdin.write("3\n")
+                    elif key == keyboard.Key.right:
+                        self.process.stdin.write("4\n")
+                    self.process.stdin.flush()
+                except AttributeError:
+                    pass
 
-
-def parse_matrix(matStr):
-    data = np.fromiter((float(x) for x in matStr.replace(';', ',').split(',')) , dtype = float)
-
-    rows = matStr.count(';') + 1
-    cols = matStr.split(';')[0].count(',') + 1
-    return data.reshape(rows, cols)
-
-
-def read_output(process):
-    while True:
-        output = process.stdout.readline()
-        if output == "" and process.poll() is not None:
-            break
-        if output:
-            data = parse_matrix(output.strip())
-            print(data, end = '     \r', flush = True)
-
-def get_focused_window_title():
-    try:
-        result = subprocess.run(['xdotool', 'getwindowfocus', 'getwindowname'], capture_output=True, text=True)
-        return result.stdout.strip()
-    except Exception as e:
-        print(f"Error getting focused window: {e}")
-        return ""
-
-def on_press(key):
-    if get_focused_window_title() == "Pendulum simulation":
-        if process.stdin:
+    def on_release(self, key) -> None:
+        if self.get_focused_window_title() == "Pendulum simulation":
             try:
-                if key == keyboard.Key.up:
-                    process.stdin.write("1\n")
-                elif key == keyboard.Key.down:
-                    process.stdin.write("2\n")
-                elif key == keyboard.Key.left:
-                    process.stdin.write("3\n")
-                elif key == keyboard.Key.right:
-                    process.stdin.write("4\n")
-                process.stdin.flush()
+                if key in {keyboard.Key.up, keyboard.Key.down, keyboard.Key.left, keyboard.Key.right} and self.process.stdin:
+                    self.process.stdin.write("0\n")  # Send reset signal on key release
+                    self.process.stdin.flush()
             except AttributeError:
                 pass
+            
+            if key == keyboard.Key.esc:
+                print("Exiting...")
+                return self.listener.stop()
 
-def on_release(key) -> None:
-    if get_focused_window_title() == "Pendulum simulation":
-        try:
-            if key in {keyboard.Key.up, keyboard.Key.down, keyboard.Key.left, keyboard.Key.right} and process.stdin:
-                process.stdin.write("0\n")  # Send reset signal on key release
-                process.stdin.flush()
-        except AttributeError:
-            pass
-        
-        if key == keyboard.Key.esc:
-            print("Exiting...")
-            return listener.stop()
-        
+    @staticmethod
+    def parse_matrix(matStr):
+        data = np.fromiter((float(x) for x in matStr.replace(';', ',').split(',')) , dtype = float)
 
-output_thread = threading.Thread(target = read_output, args = (process, ))
-output_thread.daemon = True
-output_thread.start()
+        rows = matStr.count(';') + 1
+        cols = matStr.split(';')[0].count(',') + 1
+        return data.reshape(rows, cols)
 
 
+    def read_output(self):
+        while True:
+            output = self.process.stdout.readline()
+            if output == "" and self.process.poll() is not None:
+                break
+            if output:
+                self.data = self.parse_matrix(output.strip())
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+    def read_data(self): return self.data
 
-process.terminate()
+    def listen(self):
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as self.listener:
+            self.listener.join()
+
+        self.process.terminate()

pysrc/modules.py
@@ -1,29 +1,17 @@
-# %% 
-# TODO: 
-# - Implement validation split (STATUS: Complete)
-# - Find a way to load a model for transfer learning. 
-#   AKA .add attribute (STATUS: Complete)
-# - Change layer argument in Sequential to accepting list 
-#   instead of *args (STATUS: Complete)
-# - Implement swish, leaky relu (STATUS: Complete)
-# - Add l1, l2 and Elastic net reg term (STATUS: Complete)
-# - AdaGrad Implementation
-# - Introduce metrics for evaluation
-# - Research on loss function and fix softmax
-# - Research on Conv2D layer and its backpropagation
 import os
-import numpy as cp
+import cupy as cp
 import pickle
-from numpy import ndarray
+from cupy import ndarray
 from typing import Any, Union, Literal
 from tqdm import tqdm
 
 class InvalidClass(Exception):
     ...
 
-
 class InputError(Exception):
     ...
+
+class InvalidActivationFunction(Exception):...
 cp.random.seed(0)
 
 # REGULARIZER
@@ -74,10 +62,20 @@ class Funct(object):
         return funct, derivative
 
     @staticmethod
+    def sigmoidWithLosss():
+        def funct(z: ndarray) -> ndarray:
+            return 1 / (1 + cp.exp(-z))
+
+        def derivative(z: ndarray) -> ndarray:
+            return cp.ones_like(z)
+
+        return funct, derivative
+
+    @staticmethod
     def softmaxs():
         def funct(z: ndarray) -> ndarray:
-            exp_z = np.exp(z - np.max(z, axis = 1, keepdims = True))
-            return exp_z / np.sum(exp_z, axis = 1, keepdims = True)
+            exp_z = cp.exp(z)
+            return exp_z / cp.sum(exp_z, axis = 0)
 
         def derivative(z: ndarray) -> ndarray:
             return cp.ones_like(z)
@@ -140,6 +138,7 @@ class Funct(object):
     swish = swishs()
     tanh = tanhs()
     sigmoid = sigmoids()
+    sigmoidWithLoss = sigmoidWithLosss()
     softmax = softmaxs()
     relu = relus()
     softplus = softpluss()
@@ -242,7 +241,7 @@ class RMSProp(object):
     def __build__(self, weights, russian_biases):
         for w, b in zip(weights, russian_biases):
             self.Sdw += [cp.zeros_like(w, dtype=cp.float64)]
-            self.Sdb += [cp.zeros_like(b, scwqdtype=cp.float64)]
+            self.Sdb += [cp.zeros_like(b, dtype=cp.float64)]
 
     def __update__(self, weights, russian_biases, nabla_ws, nabla_bs):
         for index in range(len(weights)):
@@ -274,9 +273,9 @@ class Dense(Funct):
         if self.optional and 'input_shape' in self.optional.keys():
             weight = cp.random.randn(
                 self.units,
-                cp.prod(cp.array(self.optional["input_shape"]))
+                int(cp.prod(cp.array(self.optional["input_shape"])))
             )
-            weight *= cp.sqrt(2 / (2 * cp.prod(self.optional["input_shape"]) + 1))
+            weight *= cp.sqrt(2 / (2 * cp.prod(cp.array(self.optional["input_shape"])) + 1))
 
         else:
             weight = cp.random.randn(
@@ -293,13 +292,21 @@ class Dense(Funct):
         expanded_inputs = cp.hstack([input, cp.atleast_2d(cp.ones(input.shape[0])).T]).T
 
         z = expanded_weights @ expanded_inputs
-        h = getattr(self, self.activation)[0](z).T
+
+        try:
+            h = getattr(self, self.activation)[0](z).T
+        except AttributeError as e:
+            raise InvalidActivationFunction(f"Activation function '{self.activation}' not found. If you're using bceloss, use sigmoid instead")
 
         return h, z
 
     def __backward__(self, dy, dot_prod, batch_input, weight):
-        
-        nabla_z = dy * getattr(self, self.activation)[1](dot_prod)
+        try:
+            nabla_z = dy * getattr(self, self.activation)[1](dot_prod)
+        except AttributeError as e:
+            print(f"Error: {e}")
+            print(f"Activation function '{self.activation}' not found.")
+            print(f"Available methods: {dir(self)}")
         nabla_w = ((nabla_z @ batch_input) - self.kernel_regularizer.__reg__(weight))
         nabla_b = cp.sum(nabla_z, axis=1, keepdims=True).T
         dy = weight.T @ nabla_z
@@ -358,11 +365,13 @@ class Sequential(object):
         self.__bias_weights += [bias]
 
     def compile(self,
-                loss: Literal['mse', 'cross_entropy_loss'] = 'mse',
+                loss: Literal['mse', 'celoss', 'bceloss'] = 'mse',
                 optimizer: Union[Adam, RMSProp, SGD] = Adam()):
         
         self.loss = loss
         self.optimizer = optimizer
+        if loss == 'bceloss':
+            self.layers[-1].activation += 'WithLoss'
         self.optimizer.__build__(self.weights, self.__bias_weights)
         self.compile_called = True
 
@@ -407,15 +416,17 @@ class Sequential(object):
         loss = cp.mean((1 / 2) * cp.sum((pred - y_truth) ** 2, axis=1))
         dy = (pred - y_truth)
         return loss, dy
-    
-    @staticmethod
-    def mae(pred, y_truth):
-        loss = cp.mean(cp.abs((pred - y_truth)), axis = 1)
         
     @staticmethod
-    def cross_entropy_loss(pred, y_truth):
+    def celoss(pred, y_truth):
         loss = - cp.mean(np.sum(y_truth * np.log(pred + 1e-5), axis = 1))
-        dy = (pred - y_truth) / y_truth.shape[1]
+        dy = pred - y_truth
+        return loss, dy
+    
+    @staticmethod
+    def bceloss(pred, y_truth):
+        loss = - cp.mean(np.sum(y_truth * np.log(pred + 1e-5) + (1 - y_truth) * np.log(1 - pred + 1e-5), axis = 1))
+        dy = pred - y_truth
         return loss, dy
 
     def fit(self, x, y, epochs: int = 1, batch_size: int = 1, validation_split: float = 0, shuffle: bool = True):